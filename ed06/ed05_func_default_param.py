9b18d1d673d340dc3e0988f8398c67acc5a31277
ODH <dh103504@naver.com>
2023-07-18 12:01:04 +0900

    파이썬

---
+++ ed06/ed05_func_default_param.py
@@ -0,0 +1,9 @@
+def buy(item1,item2='음료수'):
+    print(item1,item2)
+
+# def profile(item2='음료수',item1):
+#     print(item1,item2)
+def main():
+    buy('아아')
+main()
+
---
+++ ed06/ed06_func.py
@@ -0,0 +1,8 @@
+def add(a,b):
+    return a+b
+def main():
+    result = add(a=12 ,b=14)
+    print(result)
+    result = add(b=14, a=12)
+    print(result)
+main()
